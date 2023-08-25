19993798bc803b2500f3808d96c0acee59a47657
HenryAnyiam <anyiamhenry7@gmail.com>
2023-08-25 05:31:09 -0700

    using puppet exec resource

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #execute a command
 exec { 'kill_a_process':
   command => 'pkill killmenow',
-  user =>'root',
-  path => ['/bin', 'usr/bin']
+  user    =>'root',
+  path    => ['/bin', 'usr/bin']
 }
