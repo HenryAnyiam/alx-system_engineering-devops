62ebd466fe2afa6b6b2fc91fb168dc6495d5249b
HenryAnyiam <anyiamhenry7@gmail.com>
2023-08-26 08:36:36 -0700

    config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,9 @@
 #client ssh comfiguration file with ssh
 file {'/root/.ssh/config':
   ensure  => file,
+  mode    => '0600',
   content => 'Host *      
       PasswordAuthentication no
-      IdentityFile ~/.ssh/school',
+      IdentityFile ~/.ssh/school
+',
 }
