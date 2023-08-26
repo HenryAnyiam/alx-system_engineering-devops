5478adc8e72dbf60a4a0b709107e926d9ec0cdeb
HenryAnyiam <anyiamhenry7@gmail.com>
2023-08-26 09:19:49 -0700

    config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line {'ensure password authentication == no':
   ensure  => present,
-  path    => '/root/.ssh/config',
+  path    => '/etc/.ssh/config',
   line    => '      PasswordAuthentication no',
   replace => true,
 }
 
 file_line {'check identity file':
   ensure  => present,
-  path    => '/root/.ssh/config',
+  path    => '/etc/.ssh/config',
   line    => '      IdentityFile ~/.ssh/school',
   replace => true,
 }
