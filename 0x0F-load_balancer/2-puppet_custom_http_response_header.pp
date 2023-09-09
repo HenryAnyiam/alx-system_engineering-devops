eeec35b7a7473e69bbe5c43ee9707b313aa5e385
HenryAnyiam <anyiamhenry7@gmail.com>
2023-09-09 06:56:14 +0100

    add customheader

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,7 +2,7 @@
 exec { 'add_header';
   command      => 'sudo apt-get -y update;
   sudo apt-get -y  install nginx;
-  sudo sed -i '/server_name _;/a \\n\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;\n\t}\n\terror_page 404 /404.html;\n\tlocation \/404.html {\n\t\tinternal;\n}' /etc/nginx/sites-available/default;
+  sudo sed -i '/server_name _;/a \\n\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default;
   sudo service nginx restart;',
   provider     => shell,
   refresh only => true,
