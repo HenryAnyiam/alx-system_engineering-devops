a55dabf4d741035afbc90733cfa098590e5651dc
HenryAnyiam <anyiamhenry7@gmail.com>
2023-09-09 04:12:51 -0700

    HAProxy

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,7 +2,7 @@
 exec { 'add_header';
   command      => 'sudo apt-get -y update;
   sudo apt-get -y  install nginx;
-  sudo sed -i '/server_name _;/a \\n\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default;
+  sudo sed -i \'/server_name _;/a \\n\tadd_header X-Served-By $hostname;\' /etc/nginx/sites-available/default;
   sudo service nginx restart;',
   provider     => shell,
   refresh only => true,
