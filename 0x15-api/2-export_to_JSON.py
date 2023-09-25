bf944cd71a9a29870101dab4c3adf1dcbd50b202
HenryAnyiam <anyiamhenry7@gmail.com>
2023-09-25 14:35:50 -0700

    serialize to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module to get employee information from REST API"""
+from requests import get
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    details = get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+    tasks = get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos")
+    data = tasks.json()
+    info = details.json()
+    name = info.get('username')
+    full = []
+    user_id = str(argv[1])
+    for i in data:
+        full.append({"task": i.get("title"),
+                     "completed": str(i.get('completed')),
+                     "username": name})
+    user = {user_id: full}
+    with open(f"{user_id}.json", 'w', encoding='UTF-8') as my_file:
+        user = json.dumps(user)
+        my_file.write(user)
