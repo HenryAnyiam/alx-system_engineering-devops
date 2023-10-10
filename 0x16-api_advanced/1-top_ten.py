7251abf2573ce27e7a386f18060a4ee91b6f1ae3
Henry Anyiam <anyiamhenry7@gmail.com>
2023-10-10 06:12:03 -0700

    fixed errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """get first ten hot posts for a given subreddit"""
     url = f"https://api.reddit.com/r/{subreddit}/hot"
     header = {'user-agent': 'my-app/0.0.1'}
-    res = requests.get(url, headers=header)
+    res = requests.get(url, headers=header, allow_redirects=False)
     if res.status_code == 404:
         print('None')
     else:
