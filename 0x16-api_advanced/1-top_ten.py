ab741b1fc676fe6574183b288b052c66c2649067
Henry Anyiam <anyiamhenry7@gmail.com>
2023-10-10 05:58:23 -0700

    subreddit titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""module with functions working with reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """get first ten hot posts for a given subreddit"""
+    url = f"https://api.reddit.com/r/{subreddit}/hot"
+    header = {'user-agent': 'my-app/0.0.1'}
+    res = requests.get(url, headers=header)
+    if res.status_code == 404:
+        print(None)
+    data = res.json()
+    titles = data.get('data').get('children')[:10]
+    for i in titles:
+        print(i.get('data').get('title'))
